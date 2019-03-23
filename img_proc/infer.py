d8e6b6abc4e1911d8b62969305ae899fdc6146ee
zukako <sakamoto.ryoma0930@gmail.com>
2019-03-23 23:17:48 +0900

    curl -F "file=@a.png"  http://0.0.0.0:3001/predict

--- img_proc/infer.py
+++ img_proc/infer.py
@@ -4,10 +4,23 @@ import cv2
 from PIL import Image
 import numpy as np
 from flask import Flask, request
-import retrieval_proc
 from io import BytesIO
 
+import matplotlib.pyplot as plt
+
+# 自作モジュールの読み込み
+import retrieval_proc
+
+# apiの宣言
 api = Flask(__name__)
+
+# [[id, 画像, 埋め込みベクトル],[id, 画像, 埋め込みベクトル], ...]の検索対象データを予め読み込んでおく
+# pickleにして保存しておいて読み込む
+import pickle 
+with open('gallery.pickle', 'rb') as g:
+  gallery = pickle.load(g)
+  print(gallery[0])
+
 
 # predictのエンドポイントのアクセスで呼ばれる関数
 # curl -F "file=@a.png"  http://0.0.0.0:3001/predict
@@ -19,9 +32,14 @@ def predict():
     print(arr.mode)
     img = np.asarray(arr.resize((128,128)).convert("RGB"))
     # 入力画像をベクトル化
-    query_img = retrieval_proc.con_embedding(img)
+    query_img_emb = retrieval_proc.con_embedding(img)
+    print(query_img_emb)
     # ランキングを算出して結果を表示
-    print(query_img)
+    ranking = retrieval_proc.calc_ranking(query_img_emb,gallery)
+
+    for r in ranking[:3]:
+        plt.imshow(Image.open(r[1]).convert("RGB"))
+        plt.show()
 
     return make_response(jsonify({"status":"success"}))
 
--- img_proc/retrieval_proc.py
+++ img_proc/retrieval_proc.py
@@ -11,12 +11,12 @@ model_epoch = 25 # 何エポックのモデルを利用するか
 con_embNet = load_model('./model/T_Shirt/krasser/con_emb_e{}.h5'.format(model_epoch))
 shop_embNet = load_model('./model/T_Shirt/krasser/shop_emb_e{}.h5'.format(model_epoch))
 
-# [[id, 画像, 埋め込みベクトル],[id, 画像, 埋め込みベクトル], ...]の検索対象データを予め読み込んでおく
-# pickleにして保存しておいて読み込む
-import pickle 
-with open('gallery.pickle', 'rb') as g:
-  gallery = pickle.load(g)
-  print(gallery[0])
+# # [[id, 画像, 埋め込みベクトル],[id, 画像, 埋め込みベクトル], ...]の検索対象データを予め読み込んでおく
+# # pickleにして保存しておいて読み込む
+# import pickle 
+# with open('gallery.pickle', 'rb') as g:
+#   gallery = pickle.load(g)
+#   print(gallery[0])
 
 # 以下の関数はinfer.pyからの呼び出しで実行される
 
@@ -34,8 +34,22 @@ def con_embedding(img):
     # plt.imshow(img)
     print(img.shape)
     img = img/255. # curlで送られてきた画像を0~1に収める。
-    plt.imshow(img) # 勝手にimshowが0~255に正規化する
-    plt.show()
+    # plt.imshow(img) # 勝手にimshowが0~255に正規化する
+    # plt.show()
     return shop_embNet.predict(np.expand_dims(img,axis=0))[0] # need [0] because of expanding dimension -> [[]]
 
 # print(con_embedding(img))
+def calc_ranking(query_img_emb,gallery):
+    print(type(gallery))
+    N = 50
+    # topN_id_list=[] # [[query1->上位20個のid],...,[queryK->上位20個のid]]
+    similarities = np.zeros(len(gallery))
+    for i,g in tqdm(enumerate(gallery)):
+        similarities[i] = distance(query_img_emb,g[2]) # retrieve from all the images
+    idxs = similarities.argsort() # ascending order/ argsort() returns indexes
+    print(idxs[:10])
+    print(type(np.array(gallery)))
+    np_gallery = np.array(gallery)
+    ranking = list(np_gallery[idxs[:N]]) #類似度の高い順にランキングしたインデックスでgalleryを切り出す
+    # topN_id_list.append(gallery[idxs[:N]]) # append ids
+    return ranking
